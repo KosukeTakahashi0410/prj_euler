b10ad6a4abfc7ed867c06d8a8b370e30ed564885
takahashikosuke <takahashikosuke@team-lab.com>
2021-05-06 16:50:37 +0900

    problem3を解答

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,18 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        // whileでとにかく計算をしてゴリ押す、何か解法が間違っている気しかしない
+        long num = 600851475143L;
+        long a = 2;
+        while(true) {
+            if(num%a==0) {
+                num = num/a;
+            }else if (a > num){
+                break;
+            }else{
+                a++;
+            }
+        }
+        System.out.println(a);
     }
 }
