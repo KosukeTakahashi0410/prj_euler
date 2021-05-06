8b46c3a8daee370e2f5998bb957734d0c110fa8f
takahashikosuke <takahashikosuke@team-lab.com>
2021-05-06 16:10:13 +0900

    problem2を解答

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,19 @@
 package com.company;
 
 public class Main {
-
+    // 総当たりな計算で算出してみる
     public static void main(String[] args) {
-	// write your code here
+        int sum = 2;
+        int a1 = 1;
+        int a2 = 2;
+        while(a2 <= 4000000) {
+            int tmp = a1 + a2;
+            if (tmp % 2 == 0) {
+                sum += tmp;
+            }
+            a1 = a2;
+            a2 = tmp;
+        }
+        System.out.println(sum);
     }
 }
