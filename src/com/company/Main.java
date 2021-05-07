f3613bafdde0509d6d25308949162f2137d38a17
takahashikosuke <takahashikosuke@team-lab.com>
2021-05-07 16:41:14 +0900

    problem7を解答

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,5 +4,22 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+        int i = 2;
+        int count = 1;
+        loop1: while(true) {
+            boolean flg = true;
+            loop2: for (int j = 2; j < i; j++) {
+                if (i%j==0) {
+                    flg = false;
+                    break loop2;
+                }
+            }
+            if(flg){
+                if(count == 10001) break loop1;
+                count ++;
+            }
+            i++;
+        }
+        System.out.println(i);
     }
 }
