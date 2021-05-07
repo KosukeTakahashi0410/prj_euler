1bbfe4126a0ac28882a46337b523a5ab10edc912
takahashikosuke <takahashikosuke@team-lab.com>
2021-05-07 15:37:25 +0900

    problem6を解答

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,5 +4,14 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+        // 総当たり計算で
+        long a = 0;
+        long b = 0;
+        for(int i=1; i<=100; i++){
+            a += Math.pow(i, 2);
+            b += i;
+        }
+        // Math.powを使いたかったが、値がうまく表示できなかったのでb*bに
+        System.out.println(b*b - a);
     }
 }
